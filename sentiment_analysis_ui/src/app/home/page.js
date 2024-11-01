d165f4249fd57bbece3904c7f2bc67e9964fef9e
Empyrealiv <123236509+Empyrealiv@users.noreply.github.com>
2024-11-02 00:43:10 +0800

    Setup basic API call

---
+++ sentiment_analysis_ui/src/app/home/page.js
@@ -0,0 +1,75 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Home() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleButtonClick = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/echo/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ message: inputValue }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Failed to send data to the API');
+        }
+    };
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                textAlign: 'center',
+            }}
+        >
+            <div style={{ padding: '20px' }}>
+                <h1 style={{ padding: '20px' }}>Sentiment Analysis</h1>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Enter text here"
+                    style={{
+                        padding: '8px',
+                        fontSize: '16px',
+                        height: '30vh',
+                        width: '100%',             
+                        verticalAlign: 'top',       
+                        resize: 'none'              
+                    }}
+                />
+            <div style={{ textAlign: 'center', marginTop: '10px' }}>
+                <button
+                    onClick={handleButtonClick}
+                    style={{
+                        padding: '8px 12px',
+                        fontSize: '16px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    Submit
+                </button>
+            </div>
+            </div>
+        </div>
+    );
+}
